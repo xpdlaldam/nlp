6f333b99f1319bd89f3c512cd4661ccbee8b8d4f
Seunghoon <xpdlaldam889@gmail.com>
2024-06-12 23:51:53 -0400

    finished stemming & lemmatization theory

--- Udemy/udemy_nlp.py
+++ Udemy/udemy_nlp.py
@@ -1,6 +1,19 @@
-### 6/10/2024 12. Stemming & Lemmatization
-## Stemming is very crude as it just chops off the end of the word => the result is not necessarily a real word
-## Lemmatization is more sophisticated as it uses actual rules of language => the true root word will be returned
+### 6/10/2024 13. Stemming & Lemmatization
+## Stemming is very crude as it just chops off the end of the word => the result is not necessarily a real word ex) better -> better
+
+## Lemmatization is more sophisticated as it uses actual rules of language => the true root word will be returned ex) better -> good
+
+## Parts-of-Speech (POS) matters as "going" can be a noun but when the root form is "go" it is not a noun  
+
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+nltk.download("wordnet")
+
+lemmatizer = WordNetLemmatizer()
+lemmatizer.lemmatize("mice") # returns "mouse"
+
+lemmatizer.lemmatize("going", pos=wordnet.NOUN) # returns "going"
+lemmatizer.lemmatize("going", pos=wordnet.VERB) # returns 'go'
 
 ### 6/10/2024 12. Stopwords
 
